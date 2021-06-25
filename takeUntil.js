0d7775458e5c890928b2d1383542e27abf19784f
jan-lab <tnlatte@outlook.com>
2021-06-24 17:59:19 -0700

    collects items from a provided array until the callback provided returns a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,65 @@
+//FUNCTION IMPLEMENTATION
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    process.exit();
+  }
+
+  let result = 1;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      result *= 1;
+    }  else {
+      result *= 0;
+    }
+
+  }
+
+  if (result === 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    //console.log(actual);
+    //console.log(expected);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const takeUntil = function(array, callback) {
+  const output = [];
+  
+  for (const item of array) {
+    //console.log("start of for loop:",callback(item))
+    if (callback(item)) {
+      //console.log(output)
+      return output;
+    }
+    output.push(item)
+  }
+  //console.log(output)
+  return output;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ])
+assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ])
