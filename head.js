0dc33339bf8ed770cb78fa47418fa98984bd736b
jan-lab <tnlatte@outlook.com>
2021-06-28 16:09:51 -0700

    modularized

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
@@ -15,10 +6,4 @@ const head = function(array) {
   }
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-//console.log(head([5,6,7]));
-//console.log(head([5]));
-//console.log(head([]));
+module.exports = head;
