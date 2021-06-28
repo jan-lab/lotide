5e1d1245190c6b80f841c5e9115554db572d47df
jan-lab <tnlatte@outlook.com>
2021-06-28 16:35:52 -0700

    modularized

--- eqArrays.js
+++ eqArrays.js
@@ -1,43 +1,3 @@
-//FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/*
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    process.exit();
-  }
-
-  let result = 1;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      result *= 1;
-    }  else {
-      result *= 0;
-    }
-  }
-
-  if (result === 1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => )should PASS
-
-*/
-
 //The following is a lot more optimized.
 
 const eqArrays = function(array1, array2) {
@@ -53,8 +13,28 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => )should PASS
+module.exports = eqArrays;
+
+
+// //The following is less optimized
+// const eqArrays = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     process.exit();
+//   }
+
+//   let result = 1;
+
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] === array2[i]) {
+//       result *= 1;
+//     }  else {
+//       result *= 0;
+//     }
+//   }
+
+//   if (result === 1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
--- tail.js
+++ tail.js
@@ -1,31 +1,8 @@
 //FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-//console.log(result)
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-// Test Case 3: Check other scenarios
-console.log(tail([1])); //An array with only one element should yield an empty array for its tail
-console.log(tail([])); //An empty array should yield an empty array for its tail
+module.exports = tail;
