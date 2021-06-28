e71338acb5162255a8fd736e1c989098e1f0bf07
jan-lab <tnlatte@outlook.com>
2021-06-28 16:40:16 -0700

    modularized

--- middle.js
+++ middle.js
@@ -1,39 +1,8 @@
 //FUNCTION IMPLEMENTATION
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    process.exit();
-  }
+const eqArrays = require('./eqArrays');
 
-  let result = 1;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      result *= 1;
-    }  else {
-      result *= 0;
-    }
-    //console.log(`array1[i]: ${array1[i]} array2[i]: ${array2[i]}`)
-    //console.log(array1[i] === array2[i])
-    //console.log(result)
-  }
-
-  if (result === 1) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    //console.log(actual);
-    //console.log(expected);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -46,14 +15,4 @@ const middle = function(array) {
   }
 };
 
-
-// TEST CODE
-assertArraysEqual(middle([1]),[]);// => []
-assertArraysEqual(middle([1, 2]),[]); // => []
-assertArraysEqual(middle([1, 2, 3]),[2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]); // => [3, 4]
-
-
-//Question: The template literal does not print out [] or ''?
+module.exports = middle;
