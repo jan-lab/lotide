3e76505970a4e43f94a193f87319bc16f543e6e2
jan-lab <tnlatte@outlook.com>
2021-06-28 21:52:34 -0700

    performs an action specified in a callback function and returns a new array

--- map.js
+++ map.js
@@ -1,29 +1,6 @@
 //FUNCTION IMPLEMENTATION
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    process.exit();
-  }
-
-  let result = 1;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      result *= 1;
-    }  else {
-      result *= 0;
-    }
-    //console.log(`array1[i]: ${array1[i]} array2[i]: ${array2[i]}`)
-    //console.log(array1[i] === array2[i])
-    //console.log(result)
-  }
-
-  if (result === 1) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -71,3 +48,5 @@ console.log(results1);
 assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']); // => true
 assertArraysEqual(map(words, word => word.length), [6,7,2,5,3]); // => true
 assertArraysEqual(map(words, word => word.split("").reverse().join("")), ['dnuorg','lortnoc','ot','rojam','mot']); // => true
+
+module.exports = map;
