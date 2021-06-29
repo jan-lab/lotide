0786c2aca22c54d62fe0f46a6e6013095487c95c
jan-lab <tnlatte@outlook.com>
2021-06-28 21:51:47 -0700

    removes the 'nests' in a nested array and returns all the elements in a single non-nested array

--- flatten.js
+++ flatten.js
@@ -1,29 +1,7 @@
 //FUNCTION IMPLEMENTATION
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    process.exit();
-  }
 
-  let result = 1;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      result *= 1;
-    }  else {
-      result *= 0;
-    }
-    //console.log(`array1[i]: ${array1[i]} array2[i]: ${array2[i]}`)
-    //console.log(array1[i] === array2[i])
-    //console.log(result)
-  }
-
-  if (result === 1) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -61,6 +39,4 @@ console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
 
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]) // => true
 
-//Question: The template literal does not print out [] or ''?
-
-//https://gist.github.com/jan-lab/b8e2bd706c9f655f17b5d2d8173e9d69
+module.exports = flatten;
